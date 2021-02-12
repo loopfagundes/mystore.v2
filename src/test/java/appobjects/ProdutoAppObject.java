c60cda9552d9a453cde1ead226074ab014ca60d6
Luciano Fagundes <luciano.fagundes@dbserver.com.br>
2021-02-12 19:10:25 -0300

    resvolido sobre iframe, alguns testes com sucesso...executei com sucesso.

--- src/test/java/appobjects/ProdutoAppObject.java
+++ src/test/java/appobjects/ProdutoAppObject.java
@@ -3,6 +3,7 @@ package appobjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class ProdutoAppObject {
     private WebDriver driver;
@@ -19,9 +20,42 @@ public class ProdutoAppObject {
         return driver.findElement(By.cssSelector("#center_column > ul > li:nth-child(5) > div > div.left-block > div > a.product_img_link > img"));
     }
 
-    public WebElement getNomeDoProdutoText() {
+    public WebElement getValidadoNomeProdutoText() {
         WebElement iframeElement = driver.findElement(By.cssSelector(".fancybox-iframe"));
         WebDriver iframeDriver = driver.switchTo().frame(iframeElement);
         return iframeDriver.findElement(By.cssSelector("#product > div > div > div.pb-center-column.col-xs-12.col-sm-4 > h1"));
+    }
+
+    public WebElement getQuantidadeProdutoText() {
+        driver.switchTo().defaultContent();
+        WebElement iframeElement = driver.findElement(By.cssSelector(".fancybox-iframe"));
+        WebDriver iframeDriver = driver.switchTo().frame(iframeElement);
+        return iframeDriver.findElement(By.id("quantity_wanted"));
+    }
+
+    public Select getTamanhoProdutoComboBox() {
+        driver.switchTo().defaultContent();
+        WebElement iframeElement = driver.findElement(By.cssSelector(".fancybox-iframe"));
+        WebDriver iframeDriver = driver.switchTo().frame(iframeElement);
+        return new Select(iframeDriver.findElement(By.id("group_1")));
+    }
+
+    public WebElement getCorProdutoButton() {
+        driver.switchTo().defaultContent();
+        WebElement iframeElement = driver.findElement(By.cssSelector(".fancybox-iframe"));
+        WebDriver iframeDriver = driver.switchTo().frame(iframeElement);
+        return iframeDriver.findElement(By.id("color_11"));
+    }
+
+    public WebElement getAdicionaCarrinhoButton() {
+        driver.switchTo().defaultContent();
+        WebElement iframeElement = driver.findElement(By.cssSelector(".fancybox-iframe"));
+        WebDriver iframeDriver = driver.switchTo().frame(iframeElement);
+        return iframeDriver.findElement(By.cssSelector("#add_to_cart > button"));
+    }
+
+    public WebElement getFazerCheckoutButton() {
+        driver.switchTo().defaultContent();
+        return driver.findElement(By.cssSelector("#layer_cart > div.clearfix > div.layer_cart_cart.col-xs-12.col-md-6 > div.button-container > a > span"));
     }
 }
--- src/test/java/tasks/ProdutoTask.java
+++ src/test/java/tasks/ProdutoTask.java
@@ -2,15 +2,18 @@ package tasks;
 
 import appobjects.ProdutoAppObject;
 import com.aventstack.extentreports.Status;
+import com.github.javafaker.Faker;
 import framework.JsExecutorFw;
 import framework.ReportFw;
 import framework.ScreenshotFw;
+import framework.WaitFw;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 
 public class ProdutoTask {
     private WebDriver driver;
     private ProdutoAppObject produtoAppObject;
+    private Faker faker = new Faker();
 
     public ProdutoTask(WebDriver driver) {
         this.driver = driver;
@@ -21,11 +24,16 @@ public class ProdutoTask {
         ReportFw.log(Status.INFO, "A pagina dos produtos.");
         validadoTelaDosProdutos();
         escolheProduto();
-        nomeDoProduto();
+        validadoNomeDoProduto();
+        quantidadeProduto();
+        tamanhoProduto();
+        corProduto();
+        adicionaNoCarrinho();
+        fazerCheckout();
     }
 
     private void validadoTelaDosProdutos() {
-        if(produtoAppObject.getValidadoTelaDosProdutosText().isDisplayed()) {
+        if (produtoAppObject.getValidadoTelaDosProdutosText().isDisplayed()) {
             ReportFw.log(Status.PASS, "A tela dos produtos esta correta.");
         } else {
             JsExecutorFw.highlight(driver, produtoAppObject.getValidadoTelaDosProdutosText());
@@ -37,13 +45,39 @@ public class ProdutoTask {
         produtoAppObject.getEscolheVestidoButton().click();
     }
 
-    private void nomeDoProduto() {
-        Assert.assertEquals("Printed Summer Dress", produtoAppObject.getNomeDoProdutoText().getText());
-        if (produtoAppObject.getNomeDoProdutoText().getText().equals("Printed Summer Dress")) {
-            ReportFw.log(Status.PASS, "O nome do produto esta correta.");
+    private void validadoNomeDoProduto() {
+        ReportFw.log(Status.INFO, "Escolhe um produto: Vestido");
+        Assert.assertEquals("Printed Summer Dress", produtoAppObject.getValidadoNomeProdutoText().getText());
+    }
+
+    private void quantidadeProduto() {
+        ReportFw.log(Status.INFO, "Quantidade do produto: 2");
+        produtoAppObject.getQuantidadeProdutoText().clear();
+        produtoAppObject.getQuantidadeProdutoText().sendKeys("2");
+    }
+
+    private void tamanhoProduto() {
+        ReportFw.log(Status.INFO, "Tamanho do produto: S como P");
+        produtoAppObject.getTamanhoProdutoComboBox().selectByValue("1");
+    }
+
+    private void corProduto() {
+        ReportFw.log(Status.INFO, "Escolhe cor do produto: Preto");
+        produtoAppObject.getCorProdutoButton().click();
+    }
+
+    private void adicionaNoCarrinho() {
+        produtoAppObject.getAdicionaCarrinhoButton().click();
+    }
+
+    private void fazerCheckout() {
+        WaitFw.visibilityOf(driver, produtoAppObject.getFazerCheckoutButton());
+        if(produtoAppObject.getFazerCheckoutButton().isDisplayed()) {
+            produtoAppObject.getFazerCheckoutButton().click();
+            ReportFw.log(Status.PASS, "A botao de Proceed to checkout recebe um clique.");
         } else {
-            JsExecutorFw.highlight(driver, produtoAppObject.getNomeDoProdutoText());
-            ReportFw.log(Status.FAIL, "O nome do produto esta incorreta. Verificado, por favor.");
+            JsExecutorFw.highlight(driver, produtoAppObject.getFazerCheckoutButton());
+            ReportFw.log(Status.FAIL, "A botao de Proceed to checkout nao recebe um clique.", ScreenshotFw.viewScreenshot(driver));
         }
     }
 }
