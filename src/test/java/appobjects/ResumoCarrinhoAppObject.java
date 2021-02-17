8ea98af8d691299e4bcdbe54170025f5fea86f09
Luciano Fagundes <luciano.fagundes@dbserver.com.br>
2021-02-17 16:57:54 -0300

    ResumoCarrinho foram sucesso!!

--- src/test/java/appobjects/ResumoCarrinhoAppObject.java
+++ src/test/java/appobjects/ResumoCarrinhoAppObject.java
@@ -18,4 +18,20 @@ public class ResumoCarrinhoAppObject {
     public WebElement getValidadoAbaResumoText() {
         return driver.findElement(By.cssSelector("#order_step > li.step_current.first > span"));
     }
+
+    public WebElement getTotalProdutoText() {
+        return driver.findElement(By.id("total_product"));
+    }
+
+    public WebElement getTotalEnvioText() {
+        return driver.findElement(By.id("total_shipping"));
+    }
+
+    public WebElement getTodoTotalText() {
+        return driver.findElement(By.id("total_price"));
+    }
+
+    public WebElement getFazerCheckoutButton() {
+        return driver.findElement(By.cssSelector("#center_column > p.cart_navigation.clearfix > a.button.btn.btn-default.standard-checkout.button-medium > span"));
+    }
 }
--- src/test/java/tasks/ResumoCarrinhoTask.java
+++ src/test/java/tasks/ResumoCarrinhoTask.java
@@ -6,6 +6,7 @@ import framework.JsExecutorFw;
 import framework.ReportFw;
 import framework.ScreenshotFw;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 
 public class ResumoCarrinhoTask {
     private WebDriver driver;
@@ -19,10 +20,16 @@ public class ResumoCarrinhoTask {
     public void ResumoCarrinho() {
         validadoTelaResumoDoCarrinho();
         validadoAbaDeResumo();
-}
+        validadoTotalProduto();
+        validadoTotalEnvio();
+        validadoTodoTotal();
+        fazerCheckout();
+    }
 
     private void validadoTelaResumoDoCarrinho() {
-        if(resumoCarrinhoAppObject.getValidadoTelaResumoCarrinhoText().getText().equals("SHOPPING-CART SUMMARY\n" +
+        Assert.assertEquals("SHOPPING-CART SUMMARY\n" +
+                "Your shopping cart contains: 2 Products", resumoCarrinhoAppObject.getValidadoTelaResumoCarrinhoText().getText());
+        if (resumoCarrinhoAppObject.getValidadoTelaResumoCarrinhoText().getText().equals("SHOPPING-CART SUMMARY\n" +
                 "Your shopping cart contains: 2 Products")) {
             ReportFw.log(Status.PASS, "A pagina de Resumo do carrinho esta correta.");
         } else {
@@ -32,11 +39,46 @@ public class ResumoCarrinhoTask {
     }
 
     private void validadoAbaDeResumo() {
-        if(resumoCarrinhoAppObject.getValidadoAbaResumoText().getText().equals("01. Summary")) {
-            ReportFw.log(Status.PASS, "A aba de \"Summary\" esta correta");
+        Assert.assertEquals("01. Summary", resumoCarrinhoAppObject.getValidadoAbaResumoText().getText());
+        if (resumoCarrinhoAppObject.getValidadoAbaResumoText().getText().equals("01. Summary")) {
+            ReportFw.log(Status.PASS, "A aba de \"Summary\" esta correta.");
         } else {
             JsExecutorFw.highlight(driver, resumoCarrinhoAppObject.getValidadoAbaResumoText());
-            ReportFw.log(Status.FAIL, "A aba de \"Summary\" esta incorreta", ScreenshotFw.viewScreenshot(driver));
+            ReportFw.log(Status.FAIL, "A aba de \"Summary\" esta incorreta.", ScreenshotFw.viewScreenshot(driver));
         }
+    }
+
+    private void validadoTotalProduto() {
+        Assert.assertEquals("$57.96", resumoCarrinhoAppObject.getTotalProdutoText().getText());
+        if (resumoCarrinhoAppObject.getTotalProdutoText().getText().equals("$57.96")) {
+            ReportFw.log(Status.PASS, "Total os produtos estao corretas.");
+        } else {
+            JsExecutorFw.highlight(driver, resumoCarrinhoAppObject.getTotalProdutoText());
+            ReportFw.log(Status.FAIL, "Total os produtos estao incorretas. Verificar, por favor.");
+        }
+    }
+
+    private void validadoTotalEnvio() {
+        Assert.assertEquals("$2.00", resumoCarrinhoAppObject.getTotalEnvioText().getText());
+        if (resumoCarrinhoAppObject.getTotalEnvioText().getText().equals("$2.00")) {
+            ReportFw.log(Status.PASS, "O valor de envio esta correta.");
+        } else {
+            JsExecutorFw.highlight(driver, resumoCarrinhoAppObject.getTotalEnvioText());
+            ReportFw.log(Status.FAIL, "O valor de envio esta incorreta, Verificar, por favor.");
+        }
+    }
+
+    private void validadoTodoTotal() {
+        Assert.assertEquals("$59.96", resumoCarrinhoAppObject.getTodoTotalText().getText());
+        if (resumoCarrinhoAppObject.getTodoTotalText().getText().equals("$59.96")) {
+            ReportFw.log(Status.PASS, "O valor de total esta correta.");
+        } else {
+            JsExecutorFw.highlight(driver, resumoCarrinhoAppObject.getTodoTotalText());
+            ReportFw.log(Status.FAIL, "O valor de total esta incorreta. Verificar, por favor.");
+        }
+    }
+
+    private void fazerCheckout() {
+        resumoCarrinhoAppObject.getFazerCheckoutButton().click();
     }
 }
